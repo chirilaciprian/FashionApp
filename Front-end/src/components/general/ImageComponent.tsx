88e1719f4469ac561cbdce137887b61d783bcd08
chirilaciprian <ciprian.chirila001@gmail.com>
2026-04-27 15:17:27 +0300

    .

--- Front-end/src/components/general/ImageComponent.tsx
+++ Front-end/src/components/general/ImageComponent.tsx
@@ -33,7 +33,7 @@ const ImageComponent: React.FC<{ image: ImageProps }> = ({ image }) => {
       width={image.width}
       effect="blur"
       onError={handleError}
-      className="object-cover w-full aspect-square"
+      className="object-cover"
       style={{ width: image.width, height: image.height }}
     />
   );
